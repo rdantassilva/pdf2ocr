acdaff21e1202f7ef4b1033160e1867ee7efeb5b
Rafael Dantas <rdantas.zip@gmail.com>
2025-05-07 12:17:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pdf2ocr",
-    version="1.0.7",
+    version="1.0.8",
     packages=find_packages(),
     install_requires=[
         'pytesseract>=0.3.10',
